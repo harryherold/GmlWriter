0655c8fa47a6230aba6f7826e34de77e644afff0
Christian Herold <harryherold@googlemail.com>
2015-01-24 17:23:48 +0100

    implemented simple test with different datatypes

--- test/test.cpp
+++ test/test.cpp
@@ -11,5 +11,20 @@ using namespace gw;
 
 int main()
 {
+	const char * file = "/home/harry/GmlWriter/test/test.gml";
+	GmlWriter g(file,true, true,"MyGraph");
+
+	g.writeNode<const char *>(1,"foo");
+	g.writeNode<double>(2,3.14);
+	g.writeNode<std::string>(3,"bar");
+	g.writeNode<int>(4,4);
+
+	g.writeEdge(1,2);
+	g.writeEdge(1,3);
+
+	g.writeEdge(2,4);
+	g.writeEdge(3,4);
+
+	std::cout << "Finished" << '\n';
 	return 0;
 }
