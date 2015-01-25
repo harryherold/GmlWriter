6293d91321a8ec285efc3efee9dc20dcfe1cfd8d
Christian Herold <harryherold@googlemail.com>
2015-01-25 18:26:21 +0100

    implemented test

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@
  *      Author: Christian Herold
  */
 #include <iostream>
+#include <string>
 #include "gml_writer.hpp"
 
 using namespace gw;
@@ -14,16 +15,20 @@ int main()
 	const char * file = "/home/harry/GmlWriter/test/test.gml";
 	GmlWriter g(file,true, true,"MyGraph");
 
-	g.writeNode<const char *>(1,"foo");
-	g.writeNode<double>(2,3.14);
-	g.writeNode<std::string>(3,"bar",Shape::Ellipse);
-	g.writeNode<int>(4,4);
+	g.writeNode(1,"foo");
+	g.writeNode(2, "3.14");
 
-	g.writeEdge(1,2);
-	g.writeEdge(1,3);
+	Shape shape_elipse = Shape::Ellipse;
 
-	g.writeEdge<double>(2,4,1.2);
-	g.writeEdge<const char *>(3,4,"baz");
+	g.writeNode(3, "bar", shape_elipse);
+	g.writeNode(4, "4");
+
+	g.writeEdge(1, 2);
+	g.writeEdge(1, 3);
+
+	EdgeStyle es = EdgeStyle::Dotted;
+	g.writeEdge(2, 4, "1.2", es);
+	g.writeEdge(3, 4, "baz", es);
 
 	std::cout << "Finished" << '\n';
 	return 0;
